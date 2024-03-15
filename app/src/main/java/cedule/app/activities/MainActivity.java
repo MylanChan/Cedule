06ae39e4923c613b187305b73e7dc7689af06dab
MylanChan <mylanchan04@gmail.com>
2024-03-15 22:21:46 +0800

    fix Notification doesn't work in high API level

--- app/src/main/java/cedule/app/activities/MainActivity.java
+++ app/src/main/java/cedule/app/activities/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.room.Room;
 
-import android.content.Context;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +15,6 @@ import cedule.app.data.Database;
 import cedule.app.fragments.CalendarFragment;
 import cedule.app.fragments.TaskFragment;
 import cedule.app.fragments.WidgetsFragment;
-import cedule.app.services.Notification;
 
 public class MainActivity extends AppCompatActivity {
     private static Database database;
@@ -75,7 +73,5 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fr_app_main, TaskFragment.class, null)
                 .commit();
-
-        new Notification(this, "test", "Test").post();
     }
 }
--- app/src/main/java/cedule/app/services/Notification.java
+++ app/src/main/java/cedule/app/services/Notification.java
@@ -1,25 +1,54 @@
 package cedule.app.services;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Color;
+
+import androidx.core.app.NotificationCompat;
 
 import cedule.app.R;
 
 public class Notification {
+    public final String CHANNEL_TASKS = "tasks";
+    public final String CHANNEL_EVENTS = "events";
+
     private final NotificationManager manager;
-    private final android.app.Notification builder;
+    private final NotificationCompat.Builder builder;
 
     public void post() {
-        manager.notify((int) System.currentTimeMillis(), builder);
+        // use current milliseconds act as unique Id
+        manager.notify((int) System.currentTimeMillis(), builder.build());
     }
 
-    public Notification(Context context, String title, String desc) {
-        manager = ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE));
+    public void createChannel(String id) {
+        // higher API level require a Notification channel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel(id, id, NotificationManager.IMPORTANCE_DEFAULT);
 
-        builder = new android.app.Notification.Builder(context)
-                        .setSmallIcon(R.drawable.ic_calendar)
-                        .setContentTitle(title)
-                        .setContentText(desc)
-                        .build();
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    public Notification(Context context, String channel, String title, String desc) {
+        manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        builder = new NotificationCompat.Builder(context, channel)
+                .setContentTitle(title)
+                .setContentText(desc);
+
+        if (channel.equals(CHANNEL_TASKS)) {
+            createChannel(channel);
+            builder.setSmallIcon(R.drawable.ic_task);
+            builder.setColor(Color.GRAY);
+            return;
+        }
+
+        if (channel.equals(CHANNEL_EVENTS)) {
+            createChannel(channel);
+            builder.setSmallIcon(R.drawable.ic_calendar);
+            builder.setColor(Color.MAGENTA);
+        }
     }
 }
