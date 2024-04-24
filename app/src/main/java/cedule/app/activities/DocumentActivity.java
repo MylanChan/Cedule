ece311eb4efa1c61f0633c31a92fcd04390fc332
MylanChan <mylanchan04@gmail.com>
2024-04-25 00:13:12 +0800

    update Layout of about us/story behind

--- app/src/main/java/cedule/app/activities/DocumentActivity.java
+++ app/src/main/java/cedule/app/activities/DocumentActivity.java
@@ -2,8 +2,10 @@ package cedule.app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import cedule.app.R;
 
@@ -13,14 +15,19 @@ public class DocumentActivity extends AppCompatActivity {
 
     private void loadHtmlByType(int type) {
         WebView wvDoc = findViewById(R.id.wv_doc);
+        wvDoc.setBackgroundColor(Color.TRANSPARENT);
+        wvDoc.getSettings().setDefaultFontSize(11);
 
         switch (type) {
             case TYPE_STORY: {
                 wvDoc.loadUrl("file:///android_asset/docs/story_behind.html");
+                ((TextView) findViewById(R.id.tv_title)).setText("Story Behind");
                 return;
             }
             case TYPE_ABOUT: {
                 wvDoc.loadUrl("file:///android_asset/docs/about_us.html");
+                ((TextView) findViewById(R.id.tv_title)).setText("About Us");
+                return;
             }
         }
     }
@@ -30,11 +37,11 @@ public class DocumentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document);
 
-        findViewById(R.id.btn_pos).setOnClickListener(v -> finish());
-
         if (getIntent() == null || getIntent().getExtras() == null) return;
 
         int type = getIntent().getExtras().getInt("type", -1);
         loadHtmlByType(type);
+
+        findViewById(R.id.ib_exit).setOnClickListener(v -> finish());
     }
 }
