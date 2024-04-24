4b316bb22f654c3309ee777d1c414ab60cf98b9c
MylanChan <mylanchan04@gmail.com>
2024-04-25 05:57:50 +0800

    update Color dialog

--- app/src/main/java/cedule/app/dialogs/ColorDialog.java
+++ app/src/main/java/cedule/app/dialogs/ColorDialog.java
@@ -1,33 +1,30 @@
 package cedule.app.dialogs;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
-import java.util.Calendar;
-
 import cedule.app.R;
-import cedule.app.activities.MainActivity;
-import cedule.app.data.entities.Category;
-import cedule.app.utils.TimeUtils;
 
 
 public class ColorDialog extends DialogFragment {
     private View view;
-    private Integer color = null;
+    private ImageButton ib = null;
 
     private void pickColor(View view) {
-        color = view.getBackgroundTintList().getDefaultColor();
+        if (ib != null) {
+            ib.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_circle));
+        }
+        ib = (ImageButton) view;
+        ib.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_circle_selected));
     }
 
     @Override @NonNull
@@ -42,7 +39,7 @@ public class ColorDialog extends DialogFragment {
 
         builder.setPositiveButton("Apply", (dialog, which) -> {
             Bundle bundle = new Bundle();
-            bundle.putInt("color", color);
+            bundle.putInt("color", ib.getImageTintList().getDefaultColor());
             requireActivity().getSupportFragmentManager().setFragmentResult("pickColor", bundle);
         });
 
@@ -50,10 +47,10 @@ public class ColorDialog extends DialogFragment {
 
         AlertDialog dialog = builder.create();
 
-        view.findViewById(R.id.ll_color_red).setOnClickListener(this::pickColor);
-        view.findViewById(R.id.ll_color_blue).setOnClickListener(this::pickColor);
-        view.findViewById(R.id.ll_color_green).setOnClickListener(this::pickColor);
-        view.findViewById(R.id.ll_color_purple).setOnClickListener(this::pickColor);
+        view.findViewById(R.id.ib_color_red).setOnClickListener(this::pickColor);
+        view.findViewById(R.id.ib_color_purple).setOnClickListener(this::pickColor);
+        view.findViewById(R.id.ib_color_blue).setOnClickListener(this::pickColor);
+        view.findViewById(R.id.ib_color_green).setOnClickListener(this::pickColor);
 
         return dialog;
     }
